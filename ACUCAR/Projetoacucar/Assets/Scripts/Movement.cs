03d9be0830c8677925239eb0e9b5184f421d3258
CARDOSO2000 <gdlaurentis.diascardoso@gmail.com>
2020-12-04 21:16:11 -0300

    EH isso

---
+++ ACUCAR/Projetoacucar/Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController2D controller2D;
+    // Start is called before the first frame update
+    float ParaOLado = 0f;
+    float velo = 40f;
+    bool pular = false;
+    bool crouch = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ParaOLado = Input.GetAxisRaw("Horizontal") * velo ;
+        if (Input.GetButtonDown("Jump"))
+        {
+            pular = true;
+        }
+        if(Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+
+        controller2D.Move(ParaOLado * Time.fixedDeltaTime, crouch, pular);
+        pular = false;
+    }
+}
